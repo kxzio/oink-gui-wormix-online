f51cd8593a519ece6ee3eb57ab11e5cd9471eec6
admin <Admin@>
2023-05-17 23:18:22 +0700

    hotkey

--- pgui/oinkui/elements/hotkey.cpp
+++ pgui/oinkui/elements/hotkey.cpp
@@ -2,6 +2,8 @@
 
 using namespace ImGui;
 
+ImGuiID active_id_hotkey = 0;
+
 bool c_oink_ui::hotkey(const char* label, keybind_t* keybind, const ImVec2& size_arg)
 {
 	same_line( );
@@ -31,30 +33,30 @@ bool c_oink_ui::hotkey(const char* label, keybind_t* keybind, const ImVec2& size
 	if (!ItemAdd(total_bb, id))
 		return false;
 
-	bool hovered, pressed_right, pressed_left;
-
-	pressed_left = ButtonBehavior(frame_bb, id, &hovered, nullptr, ImGuiButtonFlags_MouseButtonLeft);
-
-	if (hovered) // process only if hovered (logic)
+	if (active_id_hotkey == 0)
 	{
-		if (!pressed_left)
-		{
-			pressed_right = ButtonBehavior(frame_bb, id, nullptr, nullptr, ImGuiButtonFlags_MouseButtonRight);
+		bool hovered, pressed_right, pressed_left;
+		pressed_left = ButtonBehavior(frame_bb, id, &hovered, nullptr, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_PressedOnRelease);
 
-			if (pressed_right) // right opened
+		if (hovered) // process only if hovered (logic)
+		{
+			if (!pressed_left)
 			{
+				pressed_right = ButtonBehavior(frame_bb, id, nullptr, nullptr, ImGuiButtonFlags_MouseButtonRight | ImGuiButtonFlags_PressedOnRelease);
 
+				if (pressed_right) // right opened
+					active_id_hotkey = id ^ 2;
+			}
+			else // left opened
+			{
+				active_id_hotkey = id;
 			};
-		}
-		else // left opened
-		{
-
 		};
 	};
 
-	bool value_changed = false;
+	const ImGuiID id_rmb = id ^ 2;
 
-	if (IsItemActive( ))
+	if (active_id_hotkey == id)
 	{
 		for (uint16_t i = 0u; i < ImGuiKey_COUNT; ++i)
 		{
@@ -63,59 +65,59 @@ bool c_oink_ui::hotkey(const char* label, keybind_t* keybind, const ImVec2& size
 				if (ImGui::IsMouseDown(static_cast<ImGuiMouseButton>(i)))
 				{
 					keybind->m_keycode = i;
-					value_changed = true;
+
 					ImGui::ClearActiveID( );
+					active_id_hotkey = 0;
+					break;
 				};
 			}
 			else
 			{
 				if (ImGui::IsKeyDown(static_cast<ImGuiKey>(i)))
 				{
-					if (i != ImGuiKey_Escape) // skip binding
-					{
+					if (i != ImGuiKey_Escape)
 						keybind->m_keycode = i;
-						value_changed = true;
-					}
 					else if (i == ImGuiKey_Backspace) // unbind
-					{
-						keybind->m_keycode = UINT16_MAX;
-						value_changed = true;
-					}
+						keybind->m_keycode = keybind_t::keybind_unbound;
 
 					ImGui::ClearActiveID( );
+					active_id_hotkey = 0;
+					break;
 				};
 			};
 		};
 	};
 
-	if (keybind->m_popup_open)
+	if (active_id_hotkey == id_rmb)
 	{
-		constexpr int m_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoScrollbar;
+		constexpr auto flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoScrollbar;
 
-		ImGui::SetNextWindowPos(ImVec2(stored_cursor_pos.x + 10 * m_dpi_scaling, stored_cursor_pos.y + 10 * m_dpi_scaling));
-		ImGui::SetNextWindowSize(ImVec2(75 * m_dpi_scaling, 82 * m_dpi_scaling));
+		ImGui::SetNextWindowPos(ImVec2(stored_cursor_pos.x + 10.f * m_dpi_scaling, stored_cursor_pos.y + 10.f * m_dpi_scaling), ImGuiCond_Appearing);
+		ImGui::SetNextWindowSize(ImVec2(85.f * m_dpi_scaling, 82.f * m_dpi_scaling), ImGuiCond_Appearing);
 
-
-		ImGui::Begin("bind_settings", NULL, m_flags);
+		if (ImGui::Begin("bind_settings", NULL, flags))
 		{
-			if (sub_button("Toogle", ImVec2(0, 0), 0, 1, keybind->m_activation_mode))
+			float avail = ImGui::GetContentRegionAvail( ).x;
+
+			if (sub_button("Toogle", ImVec2(avail, 0), 0, keybind_mode_toggle, keybind->m_activation_mode))
 				keybind->m_activation_mode = keybind_mode_toggle;
-			if (sub_button("On Press", ImVec2(0, 0), 0, 2, keybind->m_activation_mode))
+			if (sub_button("On Press", ImVec2(avail, 0), 0, keybind_mode_onpress, keybind->m_activation_mode))
 				keybind->m_activation_mode = keybind_mode_onpress;
-			if (sub_button("Always", ImVec2(0, 0), 0, 3, keybind->m_activation_mode))
+			if (sub_button("Always", ImVec2(avail, 0), 0, keybind_mode_always_on, keybind->m_activation_mode))
 				keybind->m_activation_mode = keybind_mode_always_on;
 
 			if (!ImGui::IsWindowFocused( ))
-				keybind->m_popup_open = false;
+			{
+				active_id_hotkey = 0;
+				ImGui::ClearActiveID( );
+			}
 		}
 		ImGui::End( );
 	}
 
-	char buf_display[64] = "None";
+	float animation_bind_select = g_ui.process_animation(label, 1, (active_id_hotkey == id || active_id_hotkey == id_rmb), 1.f, 13.f, e_animation_type::animation_dynamic);
 
-	float animation_bind_select = g_ui.process_animation(label, 2, g.ActiveId == id, 21, 13.f, e_animation_type::animation_dynamic);
-
-	ImColor bg_new_colour = m_theme_colour_primary; bg_new_colour.Value.w = (20.f + (animation_bind_select / 21.f) * 200) / 255.f;
+	ImColor bg_new_colour = m_theme_colour_primary; bg_new_colour.Value.w = animation_bind_select;
 
 	window->DrawList->AddRectFilled(frame_bb.Min + ImVec2(1, 1), frame_bb.Max - ImVec2(1, 1), bg_new_colour, style.FrameRounding);
 	//outline
@@ -123,44 +125,79 @@ bool c_oink_ui::hotkey(const char* label, keybind_t* keybind, const ImVec2& size
 	bg_new_colour.Value.w = 150.f / 255.f;
 	window->DrawList->AddRect(frame_bb.Min, frame_bb.Max, bg_new_colour, style.FrameRounding);
 
-	if (keybind->m_keycode != 0 && g.ActiveId != id)
-		strcpy_s(buf_display, m_key_names[keybind->m_keycode]);
-	else if (g.ActiveId == id)
-		strcpy_s(buf_display, "None");
+	const char* sz_display = "None";
+
+	if (keybind->m_activation_mode == keybind_mode_always_on)
+		sz_display = "Always";
+	else if (keybind->m_keycode != keybind_t::keybind_unbound)
+	{
+		if (keybind->m_keycode < ImGuiMouseButton_COUNT)
+		{
+			sz_display = "Left Mouse";
+
+			switch (keybind->m_keycode)
+			{
+				case 0:
+					break;
+				case 1:
+					sz_display = "Right Mouse";
+					break;
+				case 2:
+					sz_display = "Middle Mouse";
+					break;
+				case 3:
+					sz_display = "X1 Button";
+					break;
+				case 4:
+					sz_display = "X2 Button";
+					break;
+				default:
+					IM_ASSERT(0);
+			};
+		}
+		else
+			sz_display = ImGui::GetKeyName(keybind->m_keycode);
+	};
 
 	const ImRect clip_rect(frame_bb.Min.x, frame_bb.Min.y, frame_bb.Min.x + size.x, frame_bb.Min.y + size.y); // Not using frame_bb.Max because we have adjusted size
 	ImVec2 render_pos = frame_bb.Min + style.FramePadding;
 
 	ImGui::PushClipRect(frame_bb.Min, frame_bb.Max, false);
-	ImGui::RenderTextClipped(frame_bb.Min + style.FramePadding + ImVec2(animation_bind_select * m_dpi_scaling, 0), frame_bb.Max - style.FramePadding + ImVec2(animation_bind_select, 0), buf_display, NULL, NULL);
+
+	ImVec2 test_size = ImGui::CalcTextSize(sz_display);
+
+	float text_slide = animation_bind_select * ((frame_bb.Max.x - frame_bb.Min.x) * 0.5f - test_size.x); // center??
+
+	ImGui::RenderTextClipped(frame_bb.Min + style.FramePadding + ImVec2(text_slide, 0.f), frame_bb.Max - style.FramePadding + ImVec2(text_slide, 0.f), sz_display, NULL, NULL);
 	ImGui::PopClipRect( );
-	\
-		if (keybind->m_activation_mode == 1)
+
+	// processing here is a bad idea
+	/*if (keybind->m_activation_mode == 1)
+	{
+		if (ImGui::IsKeyDown(keybind->m_keycode) && ImGui::IsKeyPressed(keybind->m_keycode))
 		{
-			if (ImGui::IsKeyDown(keybind->m_keycode) && ImGui::IsKeyPressed(keybind->m_keycode))
+			keybind->m_active = !keybind->m_active;
+			return true;
+		}
+	}
+	else
+		if (keybind->m_activation_mode == 2)
+		{
+			if (ImGui::IsKeyDown(keybind->m_keycode))
 			{
-				keybind->m_active = !keybind->m_active;
+				keybind->m_active = true;
 				return true;
 			}
 		}
 		else
-			if (keybind->m_activation_mode == 2)
+			if (keybind->m_activation_mode == 3)
 			{
-				if (ImGui::IsKeyDown(keybind->m_keycode))
-				{
-					keybind->m_active = true;
-					return true;
-				}
+				keybind->m_active = true;
+				return true;
 			}
-			else
-				if (keybind->m_activation_mode == 3)
-				{
-					keybind->m_active = true;
-					return true;
-				}
 
 	if (keybind->m_keycode != 0 && keybind->m_activation_mode != 1)
-		keybind->m_active = false;
+		keybind->m_active = false;*/
 
 	return false;
 }
